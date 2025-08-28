9a53a8df9c75f2df28f53d1bdfefe6db592734df
reemRB <reem.bostami98@outlook.com>
2025-08-29 01:25:08 +0400

    refactor(app): switch to standalone component and update change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
+
     provideRouter(routes),
     provideClientHydration(),
   ],
