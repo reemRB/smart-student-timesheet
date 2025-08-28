04a42066369ff37c170e0eaf70aa343017f29da4
reemRB <reem.bostami98@outlook.com>
2025-08-28 21:53:14 +0400

    chore(add-ci): add ci commands + fail lint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideClientHydration(),
   ],
 };
