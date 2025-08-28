f726965a71958afae16c25d98b73d80d53a3eda2
reemRB <reem.bostami98@outlook.com>
2025-08-29 00:30:51 +0400

    fix(student-dashboard): add StudentDetails and StudentEntry components to module imports

--- src/app/student-dashboard/student-dashboard.module.ts
+++ src/app/student-dashboard/student-dashboard.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { StudentDashboardRoutingModule } from './student-dashboard-routing.module';
 import { StudentDashboardComponent } from './student-dashboard.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
 
 @NgModule({
-  imports: [StudentDashboardRoutingModule],
-  providers: [],
+  imports: [
+    StudentDashboardRoutingModule,
+    StudentDetailsComponent,
+    StudentEntryComponent,
+  ],
   declarations: [StudentDashboardComponent],
 })
 export class StudentDashboardModule {}
