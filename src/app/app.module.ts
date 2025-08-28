8e51fc9ecd38cdc068f121f4f7d883b8cfd299a2
reemRB <reem.bostami98@outlook.com>
2025-08-29 01:14:16 +0400

    fix(app): remove standalone from app

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+})
+export class AppModule {}
--- src/app/student-dashboard/student-dashboard-routing.module.ts
+++ src/app/student-dashboard/student-dashboard-routing.module.ts
@@ -1,12 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { StudentDashboardComponent } from './student-dashboard.component';
 import { NgModule } from '@angular/core';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: StudentDashboardComponent,
-    // children: [{ path: '', component: StudentEntryComponent }],
+    children: [{ path: '', component: StudentEntryComponent }],
     //resolve
   },
 ];
