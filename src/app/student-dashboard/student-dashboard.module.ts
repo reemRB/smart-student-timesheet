2ad57034a11cc2bc430902b15a6e251b86979555
reemRB <reem.bostami98@outlook.com>
2025-08-29 01:01:18 +0400

    fix(student-dashboard): remove unused StudentDetails and StudentEntry component imports

--- src/app/student-dashboard/student-dashboard.module.ts
+++ src/app/student-dashboard/student-dashboard.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { StudentDashboardRoutingModule } from './student-dashboard-routing.module';
 import { StudentDashboardComponent } from './student-dashboard.component';
-import { StudentDetailsComponent } from './student-details/student-details.component';
-import { StudentEntryComponent } from './student-entry/student-entry.component';
 
 @NgModule({
-  imports: [
-    StudentDashboardRoutingModule,
-    StudentDetailsComponent,
-    StudentEntryComponent,
-  ],
+  imports: [StudentDashboardRoutingModule],
   declarations: [StudentDashboardComponent],
 })
 export class StudentDashboardModule {}
