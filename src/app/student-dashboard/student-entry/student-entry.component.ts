405a94dd8b999269456a20bc862e7d2dc5e9aedb
reemRB <reem.bostami98@outlook.com>
2025-08-31 04:55:27 +0400

    feat(student-entry): implement student entry form with input and submit button

--- src/app/student-dashboard/student-entry/student-entry.component.ts
+++ src/app/student-dashboard/student-entry/student-entry.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-student-entry',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './student-entry.component.html',
   styleUrl: './student-entry.component.scss',
 })
-export class StudentEntryComponent {}
+export class StudentEntryComponent {
+  studentId: string = '';
+}
