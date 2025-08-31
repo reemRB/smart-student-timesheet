9b0f5cae6ae085a4843456504ed018cb7466ae52
reemRB <reem.bostami98@outlook.com>
2025-08-31 04:42:49 +0400

    feat(student-details): implement student card and timesheet table components in student details view

--- src/app/student-dashboard/student-details/student-details.component.ts
+++ src/app/student-dashboard/student-details/student-details.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { StudentCardComponent } from './student-card/student-card.component';
+import { StudentTimesheetTableComponent } from './student-timesheet-table/student-timesheet-table.component';
 
 @Component({
   selector: 'app-student-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StudentCardComponent, StudentTimesheetTableComponent],
   templateUrl: './student-details.component.html',
   styleUrl: './student-details.component.scss',
 })
