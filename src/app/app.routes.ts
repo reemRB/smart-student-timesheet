2a4e0915af5f9664c7949ad62d022f4ea222a971
reemRB <reem.bostami98@outlook.com>
2025-08-31 14:02:54 +0400

    feat(routes): add default and wildcard routes for student redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'student',
+    pathMatch: 'full',
+  },
+  {
     path: 'student',
     loadChildren: () =>
       import('./student-dashboard/student-dashboard.routes').then(
         (m) => m.routes,
       ),
+  },
+  {
+    path: '**',
+    redirectTo: 'student',
   },
 ];
