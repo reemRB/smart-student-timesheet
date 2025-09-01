5953e5c904f4868b008bd811d6f713b5d19f756a
reemRB <reem.bostami98@outlook.com>
2025-09-01 13:46:51 +0400

    fix(student-dashboard.routes): add student resolver for student details route

--- src/app/student-dashboard/student-dashboard.routes.ts
+++ src/app/student-dashboard/student-dashboard.routes.ts
@@ -1,6 +1,7 @@
 import { StudentDetailsComponent } from './student-details/student-details.page';
 import { StudentDashboardComponent } from './student-dashboard.component';
 import { StudentEntryComponent } from './student-entry/student-entry.page';
+import { studentResolver } from './student-details/core/student-details.resolver';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
       {
         path: ':studentId',
         component: StudentDetailsComponent,
+        resolve: { student: studentResolver },
       },
     ],
   },
